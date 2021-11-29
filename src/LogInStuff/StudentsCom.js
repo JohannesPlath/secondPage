62c89bb63d9649ea991d1144948c89aaa716e880
s0575347 <johannes.plath@student.htw-berlin.de>
2021-11-29 12:35:11 +0100

    farbigePasswortabfrage Optimiert

--- src/LogInStuff/StudentsCom.js
+++ src/LogInStuff/StudentsCom.js
@@ -8,7 +8,7 @@ var Students = [
         "DOB  ": "1995/08/25",
         "Department": "IT",
         "EmailID ": "tomholland@gmail.com",
-        "JoiningYr " : "2019"
+        "JoiningYr " : "2019.10.01"
     },
     {
         "ID": "3",
@@ -18,7 +18,7 @@ var Students = [
         "DOB": "1990/07/25",
         "Department": "IT",
         "EmailID ": "joesyH@gmail.com",
-        "JoiningYr ": "2019"
+        "JoiningYr ": "2019.10.01"
     },
     {
         "ID": "4",
@@ -28,7 +28,7 @@ var Students = [
         "DOB": "1995/07/25",
         "Department": "Maschienenbau",
         "EmailID ": "joeharry@gmail.com",
-        "JoiningYr ": "2020"
+        "JoiningYr ": "2020.04.01"
     }, {
         "ID": "5",
         "FirstName": "Pitt",
@@ -37,7 +37,7 @@ var Students = [
         "DOB": "1994/07/25",
         "Department": "WEB",
         "EmailID ": "PiPy@gmail.com",
-        "JoiningYr ": "2021"
+        "JoiningYr ": "2021.04.01"
     },
     {
         "ID": "6",
@@ -47,7 +47,7 @@ var Students = [
         "DOB": "1994/07/25",
         "Department": "Maschienenbau",
         "EmailID ": "joeharry@gmail.com",
-        "JoiningYr ": "2020"
+        "JoiningYr ": "2020.10.01"
     },
     {
         "ID": "7",
@@ -57,7 +57,7 @@ var Students = [
         "DOB": "1998/07/25",
         "Department": "IT",
         "EmailID ": "joesyH@gmail.com",
-        "JoiningYr ": "2020"
+        "JoiningYr ": "2020.04.01"
     },
     {
         "ID": "8",
@@ -67,13 +67,62 @@ var Students = [
         "DOB": "1980/07/25",
         "Department": "WEB",
         "EmailID ": "BeBro@gmail.com",
-        "JoiningYr ": "2020"
-    }
+        "JoiningYr ": "2020.10.01"
+    },
+    {
+        "ID": "9",
+        "FirstName": "Tomas",
+        "    LastName " :"Cock ",
+        "Gender": "1",
+        "DOB  ": "1995/08/25",
+        "Department": "IT",
+        "EmailID ": "tomcock@gmail.com",
+        "JoiningYr " : "2019.10.01"
+    }, {
+        "ID": "10",
+        "FirstName": "Patty",
+        "LastName ": "Poo",
+        "Gender": "2",
+        "DOB": "1994/05/05",
+        "Department": "WEB",
+        "EmailID ": "papoo@gmail.com",
+        "JoiningYr ": "2019.04.01"
+    },
+    {
+        "ID": 11,
+        "FirstName": "Frederick",
+        "LastName ": "Ohlsen",
+        "Gender": "1",
+        "DOB": "1980/06/25",
+        "Department": "Maschienenbau",
+        "EmailID ": "dragQuee69@gmail.com",
+        "JoiningYr ": "2018.10.01"
+    },
+    {
+        "ID": "12",
+        "FirstName": "Philippa",
+        "LastName ": "Morrrisset",
+        "Gender": "2",
+        "DOB": "1998/10/25",
+        "Department": "IT",
+        "EmailID ": "phimo@yahoo.com",
+        "JoiningYr ": "2020.04.01"
+    }, {
+        "ID": "13",
+        "FirstName": "Marty",
+        "LastName ": "McFly",
+        "Gender": "1",
+        "DOB": "1994/07/25",
+        "Department": "WEB",
+        "EmailID ": "marfly@gmail.com",
+        "JoiningYr ": "2020.10.01"
+    },
+
 
 ]
 function showSelectedList(){
-    if (typeof document.getElementById("SS/WS") == "Summer")
-        filterJoiningYear(Students, )
+    let element = document.getElementById("SS/WS")
+    filterJoiningYear(Students, element.value)
 }
 
 
@@ -94,13 +143,29 @@ function filterDepartment(students, Department) {
 }
 
 function filterJoiningYear(students, Semester){
+    console.log("ilterJoiningYear(students, Semester");
     if(Semester==="Summer"){
         return students.filter(function (e){
             return e.JoiningDate.slice(5,7) >= 4 && e.JoiningDate.slice(5,7) <=9
-        });} else if (Semester==="Winter"){
+        });
+    } else if (Semester==="Winter"){
         return students.filter(function (e){
             return e.JoiningDate.slice(5,7) >= 9 || e.JoiningDate.slice(5,7) <=1
         });
     }
 
+}
+
+
+
+
+function requestJson() {
+    const xhttp = new XMLHttpRequest()
+    xhttp.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            console.log(xhttp.responseText)
+        }
+    };
+    xhttp.open('GET', 'https://jsonplaceholder.typicode.com/users', true);
+    xhttp.send();
 }
